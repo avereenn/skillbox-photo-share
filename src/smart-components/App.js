20ab714e3c2545cde66db0eac072cb79d555a970
igor_averin@mobile <awerig47@gmail.com>
2022-03-30 12:41:10 +0300

    added src structure, components and files for customization

---
+++ src/smart-components/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ImageList from '../components/imageList.js';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+}
