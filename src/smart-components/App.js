27ae328bbe63ae7ac09c52fb1c4d0e9f38354e6d
igor averin@ubuntu <awerig47@gmail.com>
2022-03-30 22:14:47 +0300

    running npm init, added dependencies, added webpack code

--- src/smart-components/App.js
+++ src/smart-components/App.js
@@ -5,4 +5,12 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
   }
+
+  render() {
+    return (
+      <div className="app__container">
+        <ImageList />
+      </div>
+    )
+  }
 }
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  mode: `development`,
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+};
